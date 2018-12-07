2e18acc880ed8b0857a53e83041d0734de405799
Elen Nikoletos <elen.nikoletos@gmail.com>
2018-12-07 18:31:45 +0100

    Add: loops

---
+++ Uebungen/08_Loops/loops.py
@@ -0,0 +1,25 @@
+# einzeiliges kommentar
+
+"""
+mehrzeiliges
+kommentar
+"""
+
+"""
+value_one = 3
+value_two = 4
+
+if value_one != value_two or value_one == value_two: #!= ungleich   == ist gleich
+    print ("Yeeha")
+else:
+    print ("Nein")
+"""
+
+for number in range(0, 5):
+    if number == 2:
+        print(number)
+        break
+    else:
+        print ("Not 2")
+
+    print ("Restcode in der Schleife")
